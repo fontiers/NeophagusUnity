95d1c29647a878246873ef2d7efbbd098d58b0c2
fontiers <166890984+fontiers@users.noreply.github.com>
2024-05-03 10:41:38 +0700

    Update PhagocyteUnity.php

--- PhagocyteUnity.php
+++ PhagocyteUnity.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2a$10$QEHmbJoGdUqzCw3kumDJXOOTwlcZugrFaY8FkEie8TXStJyxzBDxC'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2y$10$kqFYJ2Nyz4zhh0D11RSqX.9.C19utc.QWpuvA8NcefBRnubHSQeYS'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
